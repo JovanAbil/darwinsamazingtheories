f9ab8cfb052cdcd384803e938a5ca83f7baadd92
Jovan <164279478+JovanAbil@users.noreply.github.com>
2026-04-12 22:44:31 -0400

    Update Page2.tsx

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -69,7 +69,7 @@ Darwin found evidence for this claim when he saw that various finch species on t
             <ScrollReveal direction="left" className="flex-1">
               {/* EDIT: Replace image */}
               <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Lucy_Mexico.jpg/250px-Lucy_Mexico.jpg?w=600&h=400&fit=crop"
+                src="https://www.genome.gov/sites/default/files/inline-images/DNA_sequencing2023.jpg?w=600&h=400&fit=crop"
                 alt="Sunlit forest path"
                 className="rounded-lg hover-pop w-full"
                 style={{ height: "350px", objectFit: "cover" }}
@@ -117,7 +117,7 @@ Fossils and DNA sequencing support common ancestry by showing physical evidence 
             <ScrollReveal direction="left" className="flex-1">
               {/* EDIT: Replace image */}
               <img
-                src="https://media.cnn.com/api/v1/images/stellar/prod/130529142644-ethiopia-lucy-skeleton-national-museum.jpg?w=600&h=400&fit=crop"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Lucy_Mexico.jpg/250px-Lucy_Mexico.jpg?w=600&h=400&fit=crop"
                 alt="Green meadow landscape"
                 className="rounded-lg hover-pop w-full"
                 style={{ height: "350px", objectFit: "cover" }}
